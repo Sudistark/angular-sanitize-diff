1439c1bbae7dd26f31c26441b8df949b5a7e1543
Ubuntu <ubuntu@ip-172-26-1-158.ap-south-1.compute.internal>
2023-05-09 05:53:38 +0000

    1st

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+
+# get all angular version list after 1.2.20 only
+
+
+versions = os.popen('npm view angular versions').read().split('\n')
+
+# format the above array properly
+
+for i in range(0, len(versions)):
+    versions[i] = versions[i].replace("'", "").replace(",", "").replace("[", "").replace("]", "").replace(" ", "")
+
+
+# loop through versions and download this file https://cdnjs.cloudflare.com/ajax/libs/angular-sanitize/1.8.3/angular-sanitize.js
+
+#I want to do a diff between all the versions so I will do it with github one by one
+
+for i in range(0, len(versions)):
+    if versions[i] > '1.2.20':
+        os.system('wget https://cdnjs.cloudflare.com/ajax/libs/angular-sanitize/' + versions[i] + '/angular-sanitize.js')
+        os.system("git add .")
+        os.system("git commit -m 'angular-sanitize.js version " + versions[i] + "'")
+        os.system("git push")
+        
+
