1768733337fc888f9000b1779736ba3eb3117318
Ubuntu <ubuntu@ip-172-26-1-158.ap-south-1.compute.internal>
2023-05-09 05:56:55 +0000

    angular-sanitize.js version

--- test.py
+++ test.py
@@ -16,7 +16,6 @@ for i in range(0, len(versions)):
 #I want to do a diff between all the versions so I will do it with github one by one
 
 for i in range(0, len(versions)):
-    if versions[i] > '1.2.20':
         os.system('wget https://cdnjs.cloudflare.com/ajax/libs/angular-sanitize/' + versions[i] + '/angular-sanitize.js')
         os.system("git add .")
         os.system("git commit -m 'angular-sanitize.js version " + versions[i] + "'")
